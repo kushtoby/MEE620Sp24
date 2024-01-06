16f816b03008da288ecd23ab1ec46a2d000499bf
bdcoller <bdcoller@gmail.com>
2024-01-06 13:31:57 -0600

    code to set up size and location of pendulum model

--- Models/StickBall.cs
+++ Models/StickBall.cs
@@ -3,15 +3,36 @@
 //============================================================================
 using Godot;
 using System;
+using System.ComponentModel.DataAnnotations;
 
 public partial class StickBall : Node3D
 {
+	MeshInstance3D stick;
+	CylinderMesh stickMesh;
+	MeshInstance3D ball;
+	SphereMesh ballMesh;
+
+	float stickLength;  // length of stick
+	float stickDiam;    // diameter of stick
+	float ballDiam;     // diameter of ball
+
+
 	//------------------------------------------------------------------------
 	// _Ready: called once
 	//------------------------------------------------------------------------
 	public override void _Ready()
 	{
 		GD.Print("StickBall:_Ready");
+
+		stickLength = 1.0f;
+		stickDiam = 0.06f;
+		ballDiam = 0.3f;
+
+		stick = GetNode<MeshInstance3D>("Stick");
+		stickMesh = (CylinderMesh)stick.Mesh;
+
+		ball = GetNode<MeshInstance3D>("Ball");
+		ballMesh = (SphereMesh)ball.Mesh;
 	}
 
 	//------------------------------------------------------------------------
@@ -20,5 +41,54 @@ public partial class StickBall : Node3D
 	//------------------------------------------------------------------------
 	public override void _Process(double delta)
 	{
+	}
+
+	//------------------------------------------------------------------------
+	// setters
+	//------------------------------------------------------------------------
+	public float Length
+	{
+		set{
+			if(value > 0.05){
+				stickLength = value;
+				stick.Position = new Vector3(0.0f, -0.5f*stickLength, 0.0f);
+				stickMesh.Height = stickLength;
+				ball.Position = new Vector3(0.0f, -stickLength, 0.0f);
+			}
+		}
+
+		get{
+			return stickLength;
+		}
+	}
+
+	public float StickDiameter
+	{
+		set{
+			if(value >= 0.001){
+				stickDiam = value;
+				stickMesh.TopRadius = 0.5f * stickDiam;
+				stickMesh.BottomRadius = 0.5f * stickDiam;
+			}
+		}
+
+		get{
+			return stickDiam;
+		}
+	}
+
+	public float BallDiameter
+	{
+		set{
+			if(value > 0.001){
+				ballDiam = value;
+				ballMesh.Radius = 0.5f * ballDiam;
+				ballMesh.Height = ballDiam;
+			}
+		}
+
+		get{
+			return ballDiam;
+		}
 	}
 }
--- Worlds/SimplePend.cs
+++ Worlds/SimplePend.cs
@@ -10,15 +10,17 @@ public partial class SimplePend : Node3D
 	float camFOV;
 	Vector3 camTg;       // coords of camera target
 
+	StickBall pModel;    // 3D model of pendulum
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		GD.Print("Hello 620");
+		GD.Print("Hello MEE 620");
 
 		// Set up the camera rig
 		longitudeDeg = 30.0f;
-		latitudeDeg = 20.0f;
-		camDist = 3.0f;
+		latitudeDeg = 15.0f;
+		camDist = 2.7f;
 
 		camTg = new Vector3(0.0f, 1.0f, 0.0f);
 		cam = GetNode<CamRig>("CamRig");
@@ -26,6 +28,16 @@ public partial class SimplePend : Node3D
 		cam.LatitudeDeg = latitudeDeg;
 		cam.Distance = camDist;
 		cam.Target = camTg;
+
+		// Set up model
+		float mountHeight = 1.4f;
+		Node3D mnt = GetNode<Node3D>("Axle");
+		mnt.Position = new Vector3(0.0f, mountHeight, 0.0f);
+		pModel = GetNode<StickBall>("StickBall");
+		pModel.Position = new Vector3(0.0f, mountHeight, 0.0f);
+		//pModel.Length = 1.1f;
+		//pModel.StickDiameter = 0.15f;
+		//pModel.BallDiameter = 0.6f;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
