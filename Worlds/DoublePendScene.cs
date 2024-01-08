f08ebbe5218303d2e11e299ba758a59978c70ef8
bdcoller <bdcoller@gmail.com>
2024-01-08 12:21:39 -0600

    camera and axle

--- Worlds/DoublePendScene.cs
+++ Worlds/DoublePendScene.cs
@@ -7,6 +7,15 @@ using System;
 
 public partial class DoublePendScene : Node3D
 {
+	// Camera Stuff
+	CamRig cam;
+	float longitudeDeg;
+	float latitudeDeg;
+	float camDist;
+	float camFOV;
+	Vector3 camTg;       // coords of camera target
+
+
 	//------------------------------------------------------------------------
 	// _Ready: Called once when the node enters the scene tree for the first 
 	//         time.
@@ -14,6 +23,20 @@ public partial class DoublePendScene : Node3D
 	public override void _Ready()
 	{
 		GD.Print("Double Pendulum Scene");
+
+		// Set up the camera rig
+		longitudeDeg = 30.0f;
+		latitudeDeg = 15.0f;
+		camDist = 2.7f;
+
+		camTg = new Vector3(0.0f, 1.0f, 0.0f);
+		cam = GetNode<CamRig>("CamRig");
+		cam.LongitudeDeg = longitudeDeg;
+		cam.LatitudeDeg = latitudeDeg;
+		cam.Distance = camDist;
+		cam.Target = camTg;
+
+		
 	}
 
 	//------------------------------------------------------------------------
