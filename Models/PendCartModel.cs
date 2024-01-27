233f49e79e28bd77cbc63eabbc20b2a9e4dcad20
bdcoller <bdcoller@gmail.com>
2024-01-27 00:58:22 -0600

    adding wheels

--- Models/PendCartModel.cs
+++ Models/PendCartModel.cs
@@ -8,11 +8,7 @@ public partial class PendCartModel : Node3D
 {
 	Node3D RootNode;
 
-	MeshInstance3D[] Wheels;
-	CylinderMesh[] WheelsMesh;
-
-	MeshInstance3D[] Pin;
-	CylinderMesh PinMesh;
+	Node3D[] Wheels;
 
 	Vector3 boxSize;
 	float wheelRad;
@@ -20,14 +16,25 @@ public partial class PendCartModel : Node3D
 
 	float pinOverhang;
 
-
+	//------------------------------------------------------------------------
+	// _Ready: called once
+	//------------------------------------------------------------------------
 	public override void _Ready()
 	{
+		GD.Print("PendCartModel Ready");
 		boxSize = new Vector3(0.5f, 0.35f, 0.5f);
 		wheelRad = 0.1f;
 		wheelThick = 0.01f;
 
-		
+		RootNode = GetNode<Node3D>("RootNode");
+		Wheels = new Node3D[4];
+		Wheels[0] = GetNode<Node3D>("RootNode/WheelNode1");
+		Wheels[1] = GetNode<Node3D>("RootNode/WheelNode2");
+		Wheels[2] = GetNode<Node3D>("RootNode/WheelNode3");
+		Wheels[3] = GetNode<Node3D>("RootNode/WheelNode4");
+
+
+		SetParams();
 	}
 
 	
@@ -35,8 +42,25 @@ public partial class PendCartModel : Node3D
 	{
 	}
 
-	private void SetSize()
+	//------------------------------------------------------------------------
+	// SetParams: Sets size parameters of the model
+	//------------------------------------------------------------------------
+	private void SetParams()
 	{
+		GD.Print("PendCartModel:SetParams");
 
+	}
+
+	//------------------------------------------------------------------------
+	// Setters
+	//------------------------------------------------------------------------
+	public float CartLength
+	{
+		set{
+			if(value > 0.05){
+				boxSize.X = value;
+				SetParams();
+			}
+		}
 	}
 }
