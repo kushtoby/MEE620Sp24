9a1b7ff7dd36fa5c392a4618aa8f481becddc812
bdcoller <bdcoller@gmail.com>
2024-01-26 22:46:23 -0600

    have a camera now

--- Worlds/PendCartScene.cs
+++ Worlds/PendCartScene.cs
@@ -8,9 +8,35 @@ using System;
 
 public partial class PendCartScene : Node3D
 {
-	// Called when the node enters the scene tree for the first time.
+	// Camera Stuff
+	CamRig cam;
+	float longitudeDeg;
+	float latitudeDeg;
+	float camDist;
+	float camFOV;
+	Vector3 camTg;       // coords of camera target
+
+	//------------------------------------------------------------------------
+	// _Ready: Called once when the node enters the scene tree for the first 
+	//         time.
+	//------------------------------------------------------------------------
 	public override void _Ready()
 	{
+		float mountHeight = 2.2f;
+
+		// Set up the camera rig
+		longitudeDeg = 30.0f;
+		latitudeDeg = 15.0f;
+		camDist = 4.0f;
+		camFOV = 55.0f;
+
+		camTg = new Vector3(0.0f, mountHeight, 0.0f);
+		cam = GetNode<CamRig>("CamRig");
+		cam.LongitudeDeg = longitudeDeg;
+		cam.LatitudeDeg = latitudeDeg;
+		cam.Distance = camDist;
+		cam.FOVDeg = camFOV;
+		cam.Target = camTg;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
