37cce800b0c6bd81258c608af6f47a54b76f6c8c
bdcoller <bdcoller@gmail.com>
2024-01-26 22:25:09 -0600

    initial commit PendCartScene

---
+++ Worlds/PendCartScene.cs
@@ -0,0 +1,20 @@
+//============================================================================
+// PendCartScene.cs
+// Code for runing the Godot scene of a planar planar pendulum attached
+// a translating cart.
+//============================================================================
+using Godot;
+using System;
+
+public partial class PendCartScene : Node3D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
