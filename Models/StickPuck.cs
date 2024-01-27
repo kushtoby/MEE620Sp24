a701e2d137254554d9e3601be38b7b668bb6b041
bdcoller <bdcoller@gmail.com>
2024-01-26 21:43:28 -0600

    Inserted code from StickBall and modified it

--- Models/StickPuck.cs
+++ Models/StickPuck.cs
@@ -21,10 +21,71 @@ public partial class StickPuck : Node3D
 	//------------------------------------------------------------------------
 	public override void _Ready()
 	{
+		stickLength = 1.0f;
+		stickThick = 0.06f;
+		puckDiam = 0.3f;
+		puckOverHang = 0.01f;
+
+		stick = GetNode<MeshInstance3D>("Stick");
+		stickMesh = (BoxMesh)stick.Mesh;
+
+		puck = GetNode<MeshInstance3D>("Puck");
+		puckMesh = (CylinderMesh)puck.Mesh;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+	}
+
+	//------------------------------------------------------------------------
+	// setters
+	//------------------------------------------------------------------------
+	public float Length
+	{
+		set{
+			if(value > 0.05){
+				stickLength = value;
+				stick.Position = new Vector3(0.0f, -0.5f*stickLength, 0.0f);
+				stickMesh.Size =  
+					new Vector3(stickThick,stickLength,stickThick);
+				puck.Position = new Vector3(0.0f, -stickLength, 0.0f);
+			}
+		}
+
+		get{
+			return stickLength;
+		}
+	}
+
+	public float StickThickness
+	{
+		set{
+			if(value >= 0.001){
+				stickThick = value;
+				stickMesh.Size =  
+					new Vector3(stickThick,stickLength,stickThick);
+				puckMesh.Height = stickThick + 2.0f*puckOverHang;
+			}
+		}
+
+		get{
+			return stickThick;
+		}
+	}
+
+	public float PuckDiameter
+	{
+		set{
+			if(value > 0.001){
+				puckDiam = value;
+				puckMesh.TopRadius = 0.5f * puckDiam;
+				puckMesh.BottomRadius = 0.5f * puckDiam;
+			}
+		}
+
+		get{
+			return puckDiam;
+		}
 	}
 }
