79c3a30ec54d30330f29765163cc2d9719d1e8ce
bdcoller <bdcoller@gmail.com>
2024-01-28 00:32:37 -0600

    data display largely working

--- Worlds/PendCartScene.cs
+++ Worlds/PendCartScene.cs
@@ -20,6 +20,10 @@ public partial class PendCartScene : Node3D
 	PendCartModel model;
 
 	// data display stuff
+	// Data display stuff
+	UIPanelDisplay datDisplay;
+	int uiRefreshCtr;     //counter for display refresh
+	int uiRefreshTHold;   // threshold for display refresh
 
 	// Mode of operation
 	enum OpMode
@@ -50,7 +54,7 @@ public partial class PendCartScene : Node3D
 		//double pendMass = 1.9;
 		//double cartMass = 2.8;
 		opMode = OpMode.SetPosition;
-		dxMan = 0.03f;
+		dxMan = 0.02f;
 		dthetaMan = 0.03f;
 		manChanged = false;
 
@@ -72,6 +76,38 @@ public partial class PendCartScene : Node3D
 		cam.Distance = camDist;
 		cam.FOVDeg = camFOV;
 		cam.Target = camTg;
+
+		// Set up data display
+		datDisplay = GetNode<UIPanelDisplay>(
+			"UINode/MarginContainer/DatDisplay");
+		datDisplay.SetNDisplay(8);
+		datDisplay.SetLabel(0,"Mode");
+		datDisplay.SetValue(0,opMode.ToString());
+		datDisplay.SetLabel(1,"Cart Pos.");
+		datDisplay.SetValue(1, 0.0f);
+		datDisplay.SetLabel(2,"Angle (deg)");
+		datDisplay.SetValue(2, 0.0f);
+		datDisplay.SetLabel(3,"Kinetic");
+		datDisplay.SetValue(3,"---");
+		datDisplay.SetLabel(4,"Potential");
+		datDisplay.SetValue(4,"---");
+		datDisplay.SetLabel(5,"Tot Energy");
+		datDisplay.SetValue(5,"---");
+		datDisplay.SetLabel(6,"Mass Ctr X");
+		datDisplay.SetValue(6,"---");
+		datDisplay.SetLabel(7,"Mass Ctr Y");
+		datDisplay.SetValue(7,"---");
+
+		datDisplay.SetDigitsAfterDecimal(1,2);
+		datDisplay.SetDigitsAfterDecimal(2,1);
+		datDisplay.SetDigitsAfterDecimal(3,4);
+		datDisplay.SetDigitsAfterDecimal(4,4);
+		datDisplay.SetDigitsAfterDecimal(5,4);
+		datDisplay.SetDigitsAfterDecimal(6,4);
+		datDisplay.SetDigitsAfterDecimal(7,4);
+
+		uiRefreshCtr = 0;
+		uiRefreshTHold = 3;
 	}
 
 	//------------------------------------------------------------------------
@@ -85,15 +121,87 @@ public partial class PendCartScene : Node3D
 			if(Input.IsActionPressed("ui_right")){
 				cartX += dxMan;
 				model.SetPositionAngle(cartX, pendAngle);
-				//GD.Print(pendAngle);
 				manChanged = true;
+				datDisplay.SetValue(1, cartX);
+				datDisplay.SetValue(3, "---");
+				datDisplay.SetValue(4, "---");
+				datDisplay.SetValue(5, "---");
 			}
 
 			if(Input.IsActionPressed("ui_left")){
 				cartX -= dxMan;
 				model.SetPositionAngle(cartX, pendAngle);
 				manChanged = true;
+				datDisplay.SetValue(1, cartX);
+				//datDisplay.SetValue(2, Mathf.RadToDeg(pendAngle));
+				datDisplay.SetValue(3, "---");
+				datDisplay.SetValue(4, "---");
+				datDisplay.SetValue(5, "---");
 			}
-		}
+
+			if(Input.IsActionJustPressed("ui_focus_next")){
+				opMode = OpMode.SetAngle;
+				datDisplay.SetValue(0, opMode.ToString());
+				//instructLabel.Text = instManual2;
+			}
+
+			if(Input.IsActionJustPressed("ui_accept")){
+				if(manChanged){
+					// sim.Angle1 = (double)pend1Rotation.Z;
+					// sim.Angle2 = (double)pend2Rotation.Z;
+					// sim.GenSpeed1 = 0.0;
+					// sim.GenSpeed2 = 0.0;
+				}
+
+				opMode = OpMode.Simulate;
+				datDisplay.SetValue(0, opMode.ToString());
+				//instructLabel.Text = instSim;
+				manChanged = false;
+			}
+			return;
+		} // if opMode.SetPosition
+
+		if(opMode == OpMode.SetAngle) {
+			if(Input.IsActionPressed("ui_right")){
+				pendAngle += dthetaMan;
+				model.SetPositionAngle(cartX, pendAngle);
+				manChanged = true;
+				datDisplay.SetValue(2, Mathf.RadToDeg(pendAngle));
+				datDisplay.SetValue(3, "---");
+				datDisplay.SetValue(4, "---");
+				datDisplay.SetValue(5, "---");
+			}
+
+			if(Input.IsActionPressed("ui_left")){
+				pendAngle -= dthetaMan;
+				model.SetPositionAngle(cartX, pendAngle);
+				manChanged = true;
+				datDisplay.SetValue(2, Mathf.RadToDeg(pendAngle));
+				datDisplay.SetValue(3, "---");
+				datDisplay.SetValue(4, "---");
+				datDisplay.SetValue(5, "---");
+			}
+
+			if(Input.IsActionJustPressed("ui_focus_next")){
+				opMode = OpMode.SetPosition;
+				datDisplay.SetValue(0, opMode.ToString());
+				//instructLabel.Text = instManual2;
+			}
+
+			if(Input.IsActionJustPressed("ui_accept")){
+				if(manChanged){
+					// sim.Angle1 = (double)pend1Rotation.Z;
+					// sim.Angle2 = (double)pend2Rotation.Z;
+					// sim.GenSpeed1 = 0.0;
+					// sim.GenSpeed2 = 0.0;
+				}
+
+				opMode = OpMode.Simulate;
+				datDisplay.SetValue(0, opMode.ToString());
+				//instructLabel.Text = instSim;
+				manChanged = false;
+			}
+			return;
+		} // if OpMode.SetAngle
 	}
 }
