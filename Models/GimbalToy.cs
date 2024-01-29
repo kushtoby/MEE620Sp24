487a8975dfd03ba8cd97d31555499e6343846d18
bdcoller <bdcoller@gmail.com>
2024-01-28 21:15:01 -0600

    Got YPR gimbal configured

--- Models/GimbalToy.cs
+++ Models/GimbalToy.cs
@@ -6,7 +6,19 @@ using System;
 
 public partial class GimbalToy : Node3D
 {
-	
+	Node3D outerRingNode;
+	Node3D middleRingNode;
+	Node3D innerRingNode;
+
+
+	enum EulerAngleMode{
+		None,
+		YPR,
+		YRP,
+		RPY,
+	}
+
+	EulerAngleMode eulerMode;
 
 	//------------------------------------------------------------------------
 	// _Ready: Called once when the node enters the scene tree for the first 
@@ -14,10 +26,34 @@ public partial class GimbalToy : Node3D
 	//------------------------------------------------------------------------
 	public override void _Ready()
 	{
+		eulerMode = EulerAngleMode.None;
+
+		outerRingNode = GetNode<Node3D>("OuterRingNode");
+		middleRingNode = GetNode<Node3D>("OuterRingNode/MiddleRingNode");
+		innerRingNode = GetNode<Node3D>
+			("OuterRingNode/MiddleRingNode/InnerRingNode");
+
+		
 	}
 
+	public void SetUp(string mm)
+	{
+		string modeString = mm.ToUpper();
 
+		if(modeString == "YPR")
+			SetUpYPR();
+	}
 
+	private void SetUpYPR()
+	{
+		eulerMode = EulerAngleMode.YPR;
+		
+		Node3D outerRing = GetNode<Node3D>("OuterRingNode/OuterRing");
+		outerRing.Rotation = new Vector3(0.5f*Mathf.Pi, 0.5f*Mathf.Pi, 0.0f);
+
+		Node3D onn = GetNode<Node3D>("OuterRingNode/OuterNubNode");
+		onn.Rotation = new Vector3(0.0f, 0.0f, 0.5f*Mathf.Pi);
+	}
 	
 	// public override void _Process(double delta)
 	// {
--- Worlds/GimbalScene.cs
+++ Worlds/GimbalScene.cs
@@ -6,6 +6,9 @@ using System;
 
 public partial class GimbalScene : Node3D
 {
+	// ModelStuff
+	GimbalToy model;
+
 	// Camera Stuff
 	CamRig cam;
 	float longitudeDeg;
@@ -20,6 +23,26 @@ public partial class GimbalScene : Node3D
 	//------------------------------------------------------------------------
 	public override void _Ready()
 	{
+
+		float ctrHeight = 1.7f;
+		model = GetNode<GimbalToy>("GimbalToy");
+		model.Position = new Vector3(0.0f, ctrHeight, 0.0f);
+		model.SetUp("Ypr");
+
+		// Set up the camera rig
+		longitudeDeg = 30.0f;
+		latitudeDeg = 15.0f;
+		camDist = 4.0f;
+		camFOV = 55.0f;
+
+		camTg = new Vector3(0.0f, ctrHeight, 0.0f);
+		cam = GetNode<CamRig>("CamRig");
+		cam.LongitudeDeg = longitudeDeg;
+		cam.LatitudeDeg = latitudeDeg;
+		cam.Distance = camDist;
+		cam.FOVDeg = camFOV;
+		cam.Target = camTg;
+
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
