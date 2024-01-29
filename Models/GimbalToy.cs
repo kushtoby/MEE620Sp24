27b370d34e6c8fbadcb21453b2a481abcfef35f3
bdcoller <bdcoller@gmail.com>
2024-01-28 19:54:24 -0600

    init commit GimbalToy

---
+++ Models/GimbalToy.cs
@@ -0,0 +1,25 @@
+//============================================================================
+// GimbalToy.cs
+//============================================================================
+using Godot;
+using System;
+
+public partial class GimbalToy : Node3D
+{
+	
+
+	//------------------------------------------------------------------------
+	// _Ready: Called once when the node enters the scene tree for the first 
+	//         time.
+	//------------------------------------------------------------------------
+	public override void _Ready()
+	{
+	}
+
+
+
+	
+	// public override void _Process(double delta)
+	// {
+	// }
+}
