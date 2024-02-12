1a0c96af6a2363a1d580c6dcf62c405f956c799e
bdcoller <bdcoller@gmail.com>
2024-02-11 23:43:39 -0600

    The new scene uJointPendScene

---
+++ Worlds/uJointPendSene.cs
@@ -0,0 +1,33 @@
+//============================================================================
+// UJointPendScene.cs
+//============================================================================
+using Godot;
+using System;
+
+public partial class uJointPendSene : Node3D
+{
+
+	// Camera Stuff
+	CamRig cam;
+	float longitudeDeg;
+	float latitudeDeg;
+	float camDist;
+	float camFOV;
+	Vector3 camTg;       // coords of camera target
+
+	//------------------------------------------------------------------------
+	// _Ready: Called once when the node enters the scene tree for the first 
+	//         time.
+	//------------------------------------------------------------------------
+	public override void _Ready()
+	{
+	}
+
+	//------------------------------------------------------------------------
+	// _Process: Called every frame. 'delta' is the elapsed time since the 
+	//           previous frame.
+	//------------------------------------------------------------------------
+	public override void _Process(double delta)
+	{
+	}
+}
