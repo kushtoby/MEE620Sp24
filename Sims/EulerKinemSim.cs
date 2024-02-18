cdf7139eb8b3eef286bce4ccf5beb12627dccfd8
bdcoller <bdcoller@gmail.com>
2024-02-18 13:54:37 -0600

    fixed the Tans

--- Sims/EulerKinemSim.cs
+++ Sims/EulerKinemSim.cs
@@ -38,13 +38,13 @@ public class EulerKinemSim : Simulator
     {
         double c1 = Math.Cos(xx[0]);       // cosine of theta1
         double s1 = Math.Sin(xx[0]);       // sine of theta1
-        double tan1 = Math.Cos(xx[0]);     // tangent of theta1
+        double tan1 = Math.Tan(xx[0]);     // tangent of theta1
         double c2 = Math.Cos(xx[1]);       // cosine of theta2
         double s2 = Math.Sin(xx[1]);       // sine of theta2
-        double tan2 = Math.Cos(xx[1]);     // tangent of theta2
+        double tan2 = Math.Tan(xx[1]);     // tangent of theta2
         double c3 = Math.Cos(xx[2]);       // cosine of theta3
         double s3 = Math.Sin(xx[2]);       // sine of theta3
-        double tan3 = Math.Cos(xx[2]);     // tangent of theta4
+        double tan3 = Math.Tan(xx[2]);     // tangent of theta4
 
         // Evaluate right sides of differential equations of motion
         // ##### You will need to provide these ###### //
@@ -61,13 +61,13 @@ public class EulerKinemSim : Simulator
     {
         double c1 = Math.Cos(xx[0]);       // cosine of theta1
         double s1 = Math.Sin(xx[0]);       // sine of theta1
-        double tan1 = Math.Cos(xx[0]);     // tangent of theta1
+        double tan1 = Math.Tan(xx[0]);     // tangent of theta1
         double c2 = Math.Cos(xx[1]);       // cosine of theta2
         double s2 = Math.Sin(xx[1]);       // sine of theta2
-        double tan2 = Math.Cos(xx[1]);     // tangent of theta2
+        double tan2 = Math.Tan(xx[1]);     // tangent of theta2
         double c3 = Math.Cos(xx[2]);       // cosine of theta3
         double s3 = Math.Sin(xx[2]);       // sine of theta3
-        double tan3 = Math.Cos(xx[2]);     // tangent of theta4
+        double tan3 = Math.Tan(xx[2]);     // tangent of theta4
 
         // Evaluate right sides of differential equations of motion
         // ##### You will need to provide these ###### //
