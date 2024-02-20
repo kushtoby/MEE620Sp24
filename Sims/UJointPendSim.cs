15312356fc5154d2ad17c43e67b568a5fbc8a9dd
Oluwatobi Kushimo <kushtoby@gmail.com>
2024-02-19 22:49:22 -0600

    U joint  pendulum update

--- Sims/UJointPendSim.cs
+++ Sims/UJointPendSim.cs
@@ -1,5 +1,5 @@
 //============================================================================
-// PendCartSim.cs   Class for creating a simulation of a the cart/pendulum.
+// UJointPendSim.cs   Class for creating a simulation of the universal pendulum.
 //============================================================================
 using System;
 
@@ -47,10 +47,14 @@ public class UJointPendSim : Simulator
 
         // Evaluate right sides of differential equations of motion
         // ##### You will need to provide these ###### //
-        ff[0] = 0.0;   // time derivative of state theta
-        ff[1] = 0.0;   // time derivative of state phi
-        ff[2] = 0.0;   // time derivative of state thetad
-        ff[3] = 0.0;   // time derivative of state phid
+
+        double cf = 0.5*(cos2Phi + 1.0);
+
+
+        ff[0] = thetad;   // time derivative of state theta
+        ff[1] = phid;   // time derivative of state phi
+        ff[2] = (sin2Phi*thetad*phid - (g*sinTheta*cosPhi/L))/cf;   // time derivative of state thetad
+        ff[3] = (-0.5*sin2Phi*thetad*thetad - (g*sinPhi*cosTheta)/L);   // time derivative of state phid
     }
 
     //------------------------------------------------------------------------
@@ -115,7 +119,16 @@ public class UJointPendSim : Simulator
             double phid   = x[3];
 
             //########## YOU NEED TO CALCULATE THIS ###########
-            return 0.0; 
+            double cosTheta = Math.Cos(theta);
+            double sinTheta = Math.Sin(theta);
+            double cosPhi = Math.Cos(phi);
+            double sinPhi = Math.Sin(phi);
+
+            double vx = L*cosTheta*phid;
+            double vy = L*(sinPhi*cosTheta*phid + sinTheta*cosPhi*thetad);
+            double vz = L*(sinPhi*sinTheta*phid - cosTheta*cosPhi*thetad);
+
+            return 0.5* m * Math.Sqrt(vx*vx + vy*vy + vz*vz); 
         }
     }
 
@@ -127,7 +140,7 @@ public class UJointPendSim : Simulator
             double phi    = x[1];
 
             //########## YOU NEED TO CALCULATE THIS ###########
-            return 0.0; 
+            return -m*g*L*Math.Cos(phi)*Math.Cos(theta); 
         }
     }
 
