e2a479634bd8b09eb5220a3bc80eb8042880fbef
bdcoller <bdcoller@gmail.com>
2024-02-23 18:55:20 -0600

    init commit TopDisModel

---
+++ Models/TopDiskModel.cs
@@ -0,0 +1,24 @@
+//============================================================================
+// StickPuck.cs  Script for customizing the model 
+//============================================================================
+using Godot;
+using System;
+
+public partial class TopDiskModel : Node3D
+{
+	Node3D PrecessNode;
+	Node3D LeanNode;
+	Node3D SpinNode;
+	
+	//------------------------------------------------------------------------
+	// _Ready: called once
+	//------------------------------------------------------------------------
+	public override void _Ready()
+	{
+	}
+
+	
+	// public override void _Process(double delta)
+	// {
+	// }
+}
