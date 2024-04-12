6a3ecadc37ef014d7f39841e1f25827d23b9144b
bdcoller <bdcoller@gmail.com>
2024-04-12 16:51:52 -0500

    Initial commit WobbleStoneModel

---
+++ Models/WobbleStoneModel.cs
@@ -0,0 +1,38 @@
+//============================================================================
+// WobbleStoneModel.cs
+//============================================================================
+using Godot;
+using System;
+
+public partial class WobbleStoneModel : Node3D
+{
+	CsgTorus3D torus;
+	CsgBox3D blockBox;
+
+	float rHoop;      // hoop radius
+	float rRim;       // rim radius       
+
+	//------------------------------------------------------------------------
+	// _Ready: called once
+	//------------------------------------------------------------------------
+	public override void _Ready()
+	{
+		GD.Print("WobbleStoneModel");
+		float rHoop = 0.75f;
+		float rRim = 0.25f;
+
+		SetSize(rHoop, rRim);
+	}
+
+	//------------------------------------------------------------------------
+	// SetSize:
+	//------------------------------------------------------------------------
+	public void SetSize(float rH, float rR)
+	{
+
+	}
+	
+	public override void _Process(double delta)
+	{
+	}
+}
