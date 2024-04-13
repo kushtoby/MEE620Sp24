336b15a2ff8e2fcda8a52ebd6deec709109cfe6c
bdcoller <bdcoller@gmail.com>
2024-04-13 01:25:06 -0500

    initial commit WeeblePlainScene

---
+++ Worlds/WeeblePlainScene.cs
@@ -0,0 +1,47 @@
+//============================================================================
+// WobbleStoneScene.cs     Scene in which we simulate the wobble stone
+//============================================================================
+using Godot;
+using System;
+
+public partial class WeeblePlainScene : Node3D
+{
+	// Model
+	WeeblePlain model;
+
+	// Camera Stuff
+	CamRig cam;
+	float longitudeDeg;
+	float latitudeDeg;
+	float camDist;
+	float camFOV;
+	Vector3 camTg;       // coords of camera target
+
+
+	//------------------------------------------------------------------------
+	// _Ready: called once
+	//------------------------------------------------------------------------
+	public override void _Ready()
+	{
+		//model = GetNode<WeeblePlain>("WeeblePlain");
+
+		// Set up the camera rig
+		longitudeDeg = 20.0f;
+		latitudeDeg = 20.0f;
+		camDist = 5.0f;
+		camFOV = 35.0f;
+
+		camTg = new Vector3(0.0f, 0.2f, 0.0f);
+		cam = GetNode<CamRig>("CamRig");
+		cam.LongitudeDeg = longitudeDeg;
+		cam.LatitudeDeg = latitudeDeg;
+		cam.Distance = camDist;
+		cam.FOVDeg = camFOV;
+		cam.Target = camTg;
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
