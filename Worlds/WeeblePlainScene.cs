51e6edd171a1a6999afa7b6bff18546c0310fe12
bdcoller <bdcoller@gmail.com>
2024-04-29 00:52:49 -0500

    got graphics interaction working

--- Worlds/WeeblePlainScene.cs
+++ Worlds/WeeblePlainScene.cs
@@ -6,8 +6,29 @@ using System;
 
 public partial class WeeblePlainScene : Node3D
 {
+	float leanDeg = 30.0f;
+	float initSpinRate = 0.0f;
+
+	enum OpMode
+	{
+		Configure,
+		Simulate,
+	}
+	OpMode opMode;         // operation mode
+
 	// Model
 	WeeblePlain model;
+	Vector3 modelPos;
+	Quaternion modelQuat;
+
+	// Simulation
+	WeeblePlainSim sim;
+	double time;
+	int nSimSteps;         // number of sim steps per _PhysicsProcess
+	float R;              // radius of weeble sphere
+
+	double[] loc;          // location of center of weeble (obtained from sim)
+	double[] quat;         // quaternion of weeble (obtained from sim)
 
 	// Camera Stuff
 	CamRig cam;
@@ -23,7 +44,23 @@ public partial class WeeblePlainScene : Node3D
 	//------------------------------------------------------------------------
 	public override void _Ready()
 	{
-		//model = GetNode<WeeblePlain>("WeeblePlain");
+		opMode = OpMode.Simulate;
+
+		model = GetNode<WeeblePlain>("WeeblePlain");
+		modelPos = new Vector3();
+		modelQuat = new Quaternion();
+
+		// Set up the simulation
+		sim = new WeeblePlainSim();
+		//sim.LeanAngle = Mathf.DegToRad(leanICDeg);
+		//sim.SpinRate = spinRate;
+
+		nSimSteps = 4;
+		time = 0.0;
+
+		R = 0.5f;
+		loc = new double[2];
+		quat = new double[4];
 
 		// Set up the camera rig
 		longitudeDeg = 20.0f;
@@ -40,8 +77,58 @@ public partial class WeeblePlainScene : Node3D
 		cam.Target = camTg;
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
+
+	//------------------------------------------------------------------------
+	// _Process: Called every frame. 'delta' is the elapsed time since the 
+	//           previous frame.
+	//------------------------------------------------------------------------
 	public override void _Process(double delta)
 	{
+		if(opMode == OpMode.Simulate){
+			double dangle = Math.Sin(time);
+			quat[0] = Math.Cos(0.5*dangle);
+			quat[1] = quat[2] = 0.0;
+			quat[3] = Math.Sin(0.5*dangle);
+
+			loc[0] = -R*dangle;
+			loc[1] = 0.0;
+
+			SetModelLoc();
+		}
+	}
+
+	//------------------------------------------------------------------------
+	// _PhysicsProcess:
+	//------------------------------------------------------------------------
+    public override void _PhysicsProcess(double delta)
+    {
+        base._PhysicsProcess(delta);
+
+		if(opMode != OpMode.Simulate)
+			return;
+
+		double subdelta = delta/((double)nSimSteps);
+		for(int i=0;i<nSimSteps;++i){
+			sim.Step(time, subdelta);
+			time += subdelta;
+		}
+    }
+
+	//------------------------------------------------------------------------
+	// SetModelLoc
+	//------------------------------------------------------------------------
+	private void SetModelLoc()
+	{
+		modelPos.X = (float)loc[0];
+		modelPos.Y = R;
+		modelPos.Z = (float)loc[1];
+
+		modelQuat.W = (float)quat[0];
+		modelQuat.X = (float)quat[1];
+		modelQuat.Y = (float)quat[2];
+		modelQuat.Z = (float)quat[3];
+
+		model.Position = modelPos;
+		model.Quaternion = modelQuat;
 	}
 }
